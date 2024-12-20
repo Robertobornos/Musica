aa2382abd24d2a94eec330de3a25a0ebde776e32
Robertobornos <robertobornos@gmail.com>
2024-12-20 16:50:32 +0100

    Add files via upload

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Musica
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pablo = new System.Windows.Forms.PictureBox();
+            this.CRO = new System.Windows.Forms.PictureBox();
+            this.duki = new System.Windows.Forms.PictureBox();
+            this.Ysya = new System.Windows.Forms.PictureBox();
+            this.Emilia = new System.Windows.Forms.PictureBox();
+            this.Nicki = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reaunudar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pablo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.duki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ysya)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Emilia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nicki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(93, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Anterior";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Anterior);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(209, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pausar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Pausar);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(332, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Siguiente";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Siguiente);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.CambiarImagen);
+            // 
+            // Pablo
+            // 
+            this.Pablo.Image = ((System.Drawing.Image)(resources.GetObject("Pablo.Image")));
+            this.Pablo.Location = new System.Drawing.Point(144, 80);
+            this.Pablo.Name = "Pablo";
+            this.Pablo.Size = new System.Drawing.Size(228, 108);
+            this.Pablo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pablo.TabIndex = 5;
+            this.Pablo.TabStop = false;
+            // 
+            // CRO
+            // 
+            this.CRO.Image = ((System.Drawing.Image)(resources.GetObject("CRO.Image")));
+            this.CRO.Location = new System.Drawing.Point(144, 80);
+            this.CRO.Name = "CRO";
+            this.CRO.Size = new System.Drawing.Size(207, 108);
+            this.CRO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CRO.TabIndex = 6;
+            this.CRO.TabStop = false;
+            // 
+            // duki
+            // 
+            this.duki.Image = ((System.Drawing.Image)(resources.GetObject("duki.Image")));
+            this.duki.Location = new System.Drawing.Point(144, 80);
+            this.duki.Name = "duki";
+            this.duki.Size = new System.Drawing.Size(193, 108);
+            this.duki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.duki.TabIndex = 7;
+            this.duki.TabStop = false;
+            // 
+            // Ysya
+            // 
+            this.Ysya.Image = ((System.Drawing.Image)(resources.GetObject("Ysya.Image")));
+            this.Ysya.Location = new System.Drawing.Point(144, 80);
+            this.Ysya.Name = "Ysya";
+            this.Ysya.Size = new System.Drawing.Size(207, 108);
+            this.Ysya.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ysya.TabIndex = 9;
+            this.Ysya.TabStop = false;
+            // 
+            // Emilia
+            // 
+            this.Emilia.Image = ((System.Drawing.Image)(resources.GetObject("Emilia.Image")));
+            this.Emilia.Location = new System.Drawing.Point(144, 80);
+            this.Emilia.Name = "Emilia";
+            this.Emilia.Size = new System.Drawing.Size(207, 108);
+            this.Emilia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Emilia.TabIndex = 10;
+            this.Emilia.TabStop = false;
+            // 
+            // Nicki
+            // 
+            this.Nicki.Image = ((System.Drawing.Image)(resources.GetObject("Nicki.Image")));
+            this.Nicki.Location = new System.Drawing.Point(144, 80);
+            this.Nicki.Name = "Nicki";
+            this.Nicki.Size = new System.Drawing.Size(207, 108);
+            this.Nicki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Nicki.TabIndex = 11;
+            this.Nicki.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(144, 207);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(228, 45);
+            this.trackBar1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // reaunudar
+            // 
+            this.reaunudar.BackColor = System.Drawing.Color.Green;
+            this.reaunudar.Location = new System.Drawing.Point(209, 315);
+            this.reaunudar.Name = "reaunudar";
+            this.reaunudar.Size = new System.Drawing.Size(75, 23);
+            this.reaunudar.TabIndex = 14;
+            this.reaunudar.Text = "Reanudar";
+            this.reaunudar.UseVisualStyleBackColor = false;
+            this.reaunudar.Click += new System.EventHandler(this.Reanudar);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.reaunudar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Nicki);
+            this.Controls.Add(this.Emilia);
+            this.Controls.Add(this.Ysya);
+            this.Controls.Add(this.duki);
+            this.Controls.Add(this.CRO);
+            this.Controls.Add(this.Pablo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(550, 413);
+            ((System.ComponentModel.ISupportInitialize)(this.Pablo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CRO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.duki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ysya)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Emilia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nicki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+       
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Pablo;
+        private System.Windows.Forms.PictureBox CRO;
+        private System.Windows.Forms.PictureBox duki;
+        private System.Windows.Forms.PictureBox Ysya;
+        private System.Windows.Forms.PictureBox Emilia;
+        private System.Windows.Forms.PictureBox Nicki;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button reaunudar;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Musica
+{
+    public partial class UserControl1 : UserControl
+    {
+        private Random random;
+        private Timer timer;
+        private ArrayList todascanciones;
+        int cont;
+        public UserControl1()
+        {   
+            InitializeComponent();
+            canciones();
+            cont = 0;
+            MostrarCancionActual();
+            random = new Random();
+
+
+            trackbar();
+
+        }
+
+
+        private void trackbar()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 100;
+            trackBar1.TickFrequency = 1;
+            duracioncancion();
+        }
+
+        private void Parartrackbar()
+        {
+           
+            timer.Stop();
+
+
+           
+        }
+
+        
+
+        private void canciones()
+        {
+            todascanciones = new ArrayList
+            {
+                "Nueva Era (part. Myke Towers)" ,
+                "Cassette 01 (Freestyle) (part. C.R.O y Neo Pistéa)",
+                "She Don't Give a FO (part. Khea)",
+                "Como Si No Importara (part. Emilia)",
+                "Ameri",
+                "Imperio (part Judeline)",
+                "Hello Cotto",
+                "Marisola (remix) (part. Standly, Cris Mj y Nicki Nicole)",
+                "Vida de Rock (part. Milo j)",
+                 "Los Del Espacio (part. LIT killah, Emilia, Tiago PZK, FMK, Rusherking, Maria Becerra y Big One)",
+                "GIVENCHY",
+                "Fifty Fifty (part. Pablo Chill-E, Obie Wanshot, Julianno Sosa, Neo Pistea & Young Cister)",
+                "hARAkiRi (part. C.R.O)",
+                "Shorty (part. Nicki Nicole)",
+                "Buscarte Lejos (part. Bizarrap)",
+                "No Me Llores (remix) (part. Leby)",
+                "Barro",
+                "Envy Me Freestyle, Pt. 2 (part. Lito Kirino y Pablo Chill-E)",
+                "Cine 🚬",
+                "PACTO (part. Neo Pistea)",
+                "Wake Up & Bake Up (feat. Wiz Khalifa & Arcángel)",
+                "En Odio",
+                "Antes de Perderte",
+                "Rockstar",
+                "antes de perderte (OG VERSION)",
+                "cONTRA><Mi (part. WE$T DUBAI)",
+                "Brindis (part. Headie One)",
+                "Goteo",
+                "QUAVO (part. Neo Pistéa y Ysy A)",
+                "Trato de Estar Bien (part. Morad)",
+                "Hardaway (part. YG y Eladio Carrion)",
+                "Mojaa (part. Bhavi)",
+                "Ella Es Una G (part. C.R.O)",
+                "Si Me Sobrara El Tiempo",
+                "Si Quieren Frontear (part. De La Ghetto y Quevedo)",
+                "Leitmotiv",
+                "Lost Tape 2016-2017 (part. C.R.O)",
+                "Si Te Sentis Sola",
+                "Un Día Más (part. YSY A)",
+                "Duki Vs Lit Killah (Semifinal 14/8)",
+                "TRAP N' EXPORT (part. Ysy A y Neo Pistea)",
+                "TROYA",
+                "Remember Me (part. KHEA y Bizarrap)",
+                "Tussi (feat. Marcianos Crew & Ysy A)",
+                "Bodak Yellow (Remix) (part. Cardi B)",
+                "Guapo (part. Ysy A, Kaktov, C.R.O y Neo Pistea)",
+                "Luna (part. Asan)",
+                "Mericrisma",
+                "RoCKSTAR 2.0 (part. Jhayco)",
+                "Sin Culpa (part. DrefQuila)",
+                "uNO dOS (part. Salastkbron)",
+                "Yin Yan (part. Rels B)",
+                "01 dE ENEro (part. Lucho SSJ)",
+                "B.U.H.O (part. Midel, Arse, Khea y Klave)",
+                "Ballin'",
+                "Celosa",
+                "Chico Estrella (part. Yesan & Asan)",
+                "don't liE (part. Quevedo)",
+                "Esto Recién Empieza (part. Emilia)",
+                "H.I.E.L.O. (part. Obie WanShot)",
+                "Lola",
+                "Malbec (part. Bizarrap)",
+                "Midtown",
+                "Sol y Luna",
+                "Te Traje Flores",
+                "24 (part. Kidd Keo y Juicy J)",
+                "Amor Bipolar (part. Mora)",
+                "JEFES DEL SUDOESTE",
+                "Luna (part. Gallagher)",
+                "Pastillas (part. Asan y Zecca)",
+                "aPoLLo13",
+                "BICI (part. AK4:20, Juhn y Juanka)",
+                "CALL ME MAYBE",
+                "Como Si Na (part. Clubhats)",
+                
+                "Escalera",
+                "Ferrari (feat. DICC)",
+                "Iba a Llamarte (part. C.R.O)",
+                
+                "Muriendome (part. KHEA)",
+                "No Drama (part. Ovi y Lucho SSJ)",
+                "No Vendo Trap",
+                "Perdón",
+                "Pintao (part. YSY A y Rei)",
+                "Shorty (part. Nicki Nicole)",
+                "Sol (part. Lara91k)",
+                "Sold Out Dates",
+                "Ultimo Tren a Ameri",
+                "Una Vaina Loca (part. Fuego & Manuel Turizo)",
+                "YaMeFui (part. Bizarrap y Nicki Nicole)",
+                "3 Estrellas En El Conjunto (part. Bizarrap & La T y La M)",
+                "5 Stars (part. C. Tangana, Neo Pistea y Polimá Westcoast)",
+                "7 Pakas (part. Chulu & C.R.O)",
+                
+                "Alas (part. C.R.O)",
+                "Amor Al Arte",
+                "Ánimo (part. Khea y Midel)",
+                "Basado En Hechos Reales (part. Homer El Mero Mero)",
+                "Billetes (part. KHEA y Neo Pistea)",
+                "bUSCANDO Ameri",
+                "Cascada",
+                "Cypher MND #16 v.1: Duki (part. Mundialista Crew y ShiGant-G)",
+                "Deja Vu (part. Big Deiv y CLUB HATS)",
+                "Exit (part Sick Luke y Lazza)",
+                "Fifty Fifty (part. Pablo Chill-E, Obie Wanshot, Julianno Sosa, Neo Pistea & Young Cister)",
+                "Fvck Luv (part. C.R.O)",
+                "Gelato (part. Eladio Carrion, Smokepurpp y Hide Miyabi)",
+                "GiGi",
+                "Hasta Las 6 (part. Kodigo, DrefQuila, JNO y SG)",
+                "Hijo de La Noche (part. C.R.O y Ysy A)",
+                "HitBoy (part. Khea)",
+                "I Don’t Know",
+                "I.D.K (part. Zanto)",
+                "It's a Vibe (part. Leebrian, C. Tangana y Khea)",
+                "JET LAG (part. Tiago PZK y LIT Killah)",
+                "LeBron",
+                "Loca (Remix) (part Bad Bunny, Cazzu y Khea)",
+                "Louis Vuitton (part. 44 KID y Mesita)",
+                "Máquina de Armado (part. Neo Pistea, Khea)",
+                "Me Gusta Lo Simple (part. Alemán)",
+                "Mi Chain de Roque",
+                "Modo Avión",
+                "Nota Especial",
+                "One Millon Dollar Baby (part. Eladio Carrión, Sfera Ebbasta)",
+                "Otro Level (part. Ysy A)",
+                "R.I.P La Manshon (Freestyle)",
+                "Rápido",
+                "Ready For The Night (feat. NahueMC)",
+                "Rosa (part. C.R.O)",
+                "Sudor y Trabajo",
+                "Top 5",
+                "Trapperz a Máfia da Sicília (part. Felp 22, Rauw Alejandro, MC Davo & Fuego)",
+                "Tumbando El Club (remix) (part. Neo Pistéa, C.R.O, Obiewanshot, Ysy A, Cazzu, Khea, Lucho SSJ, Coqeéin Montana y Marcianos Crew)"
+                ,"Unfollow (part. Justin Quiles y Bizarrap)",
+                "La Vuelta",
+                "Acapella",
+                "AEROBICO (remix) (part. BHAVI, MILO J y LIT killah)",
+                "ANGELITO (part. Obie Wanshot)",
+                "Baby Pluto (part. Mesita)",
+                "Batman",
+                "Café (part. Luyo y Fuego)",
+                "Cara de Diablo (part. Midel)",
+                "Carita Morena (part. Omar Montes)",
+                "Cassette 01 (Freestyle) (part. C.R.O y Neo Pistéa)",
+                "Cayendo Pa' Arriba",
+                "CEO (part. Mesita)",
+                "Cereza (part. Fuego)",
+                "Colombiana (part. Marcianos Crew y Homer El Mero Mero)",
+                "Como Si No Importara (part. Emilia)",
+                "Cuanto (part. Lucho Ssj y Farina)",
+                "Cypher MND #16 v.2: Duki (part. Mundialista Crew y ShiGant-G)",
+                "DEL ESTUDIO AL ESTADIO (part. Seven Kayne)",
+                "Dolce (part. Young Cister)",
+                "DUKI (BZRP Music Sessions #50)",
+                "Ella Es Mi Bitch (part. Pekeño 77, Mesita, 44 Kid & Franux BB)",
+                "En Movimiento",
+                "Entre Cuatro Paredes (part. Vicentico, La Bomba de Tiempo)",
+                "Envy Me Freestyle, Pt. 2 (part. Lito Kirino y Pablo Chill-E)",
+                "EO EO (part. Bles y Dellaflame)",
+                "Eres Para Mí (part. Lara 91k)",
+                "Flex Like Trunkz (part. Young Cister)",
+                "FORNAI (part. Orodembow)",
+                "Hablamos Mucho Hacemos Poco (part. Seven Kayne)",
+                "Hambre de Más (part. Klan y YSY A)",
+                "Hello Cotto (Remix) (part. Moonkey)",
+                "I Dance (feat. YG & Cuco) [explicit]",
+                "La Jefatura (part. Lucho SSJ, Marcianos Crew)",
+                "Lambo (remix) (part. C.R.O., Chucky 73, We$t Dubai y Moonkey)",
+                "Level Up",
+                "Lex Luthor (part. C.R.O.)",
+                "Ley de Atracción",
+                "Lo Olvidaste",
+                "Loca (part. Cazzu y Khea)",
+                "Malbec (part. Bizarrap) (GALLERY SESSION)",
+                "Marca de La Cadena (part. ByMonkid)",
+                "Marisola (remix) (part. Standly, Cris Mj y Nicki Nicole)",
+                "MASCARA (part. NICKI NICOLE)",
+                "Me Dejaste Así",
+                "Me Enseñaste (remix) (part. Sael)",
+                "Mi Diablo",
+                "Mil Colores",
+                "N.C.L.C",
+                "NO DA MÁS (part. YSY A)",
+                "NO SON KLLE (part. Santa Fe Klan y Peso Pluma)",
+                "Otra Vez",
+                "Pantera",
+                
+                "Perdiendo El Tiempo (part. Marcianos Crew y Homer)",
+                "Perrea (part. Frijo y We$t Dubai)",
+                "Perreo Bendito",
+                "Por Mi Nombre (part. Club Hats)",
+                "RCP (part. Tiago PZK)",
+                "Sake (part. WE$T DUBAI)",
+                "Salgo Pa La Calle (part. Mesita)",
+                "Secreto (part. Guayanaa, Farina)",
+                "Señorita"
+            };
+        }
+
+        private void MostrarCancionActual()
+        {
+            if (todascanciones.Count > 0 && cont >= 0 && cont < todascanciones.Count)
+            {
+                label1.Text = "Reproduciendo:"+ todascanciones[cont];
+            }
+
+          
+            
+        }
+
+   
+
+      
+
+        private void Anterior(object sender, EventArgs e)
+        {
+            if (cont > 0)
+            {
+                cont--;
+                MostrarCancionActual();
+            }
+            else
+            {
+                MessageBox.Show("No hay canciones anteriores");
+            }
+            trackBar1.Value = 0;
+            duracioncancion();
+        }
+
+        private void Pausar(object sender, EventArgs e)
+        {
+            MessageBox.Show("Reproducción pausada.");
+            Parartrackbar();
+        }
+
+
+
+
+
+        private void Siguiente(object sender, EventArgs e)
+        {
+            if (cont < todascanciones.Count - 1)
+            {
+                cont++;
+                MostrarCancionActual();
+            }
+            else
+            {
+                MessageBox.Show("No hay más canciones");
+            }
+            trackBar1.Value = 0;
+            duracioncancion();
+
+
+        }
+
+        private void CambiarImagen(object sender, EventArgs e)
+        {
+
+            if (!label1.Text.Contains("DUKI") || !label1.Text.Contains("duki"))
+            {
+
+                Emilia.Visible = false;
+                Nicki.Visible = false;
+                Ysya.Visible = false;
+                duki.Visible = true;
+                Pablo.Visible = false;
+                CRO.Visible = false;
+
+
+
+                if (label1.Text.Contains("C.R.O"))
+                {
+
+                    Emilia.Visible = false;
+                    Nicki.Visible = false;
+                    Ysya.Visible = false;
+                    duki.Visible = false;
+                    Pablo.Visible = false;
+                    CRO.Visible= true;
+
+
+                }
+
+
+                if (label1.Text.Contains("Emilia"))
+                {
+
+                    Emilia.Visible = true;
+                    Nicki.Visible = false;
+                    Ysya.Visible = false;
+                    duki.Visible = false;
+                    Pablo.Visible = false;
+                    CRO.Visible = false;
+
+
+                }
+
+
+                if (label1.Text.Contains("Nicki"))
+                {
+
+                    Emilia.Visible = false;
+                    Nicki.Visible = true;
+                    Ysya.Visible = false;
+                    duki.Visible = false;
+                    Pablo.Visible = false;
+                    CRO.Visible = false;
+
+
+                }
+
+
+                if (label1.Text.Contains("Ysy") || label1.Text.Contains("YSY"))
+                {
+
+                    Emilia.Visible = false;
+                    Nicki.Visible = false;
+                    Ysya.Visible = true;
+                    duki.Visible = false;
+                    Pablo.Visible = false;
+                    CRO.Visible = false;
+
+                }
+
+
+                if (label1.Text.Contains("Pablo") || label1.Text.Contains("Chill-E"))
+                {
+
+                    Emilia.Visible = false;
+                    Nicki.Visible = false;
+                    Ysya.Visible = false;
+                    duki.Visible = false;
+                    Pablo.Visible =true;
+                    CRO.Visible = false;
+
+                }
+
+            }
+          
+
+
+
+
+        }
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+           
+            if (trackBar1.Value < trackBar1.Maximum)
+            {
+                trackBar1.Value++;
+            }
+
+          
+          
+        }
+
+
+        public void duracioncancion()
+
+        {
+            label2.Text = random.Next(1, 5).ToString();
+
+        }
+
+       
+        private void Reanudar(object sender, EventArgs e)
+        {
+            trackbar();
+        }
+    }
+}
